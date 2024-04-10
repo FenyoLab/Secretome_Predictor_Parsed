40e1ef9382af239af6e65b7b2ab6fa47424b209b
amit-bio <amit.bhardwaj2@nyulangone.org>
2024-04-10 10:29:20 -0400

    .

--- weights.py
+++ weights.py
@@ -1,5 +1,5 @@
-correct_secreted = [0.975,0.966,0.972,0.998,1,1]
-correct_nonsecreted = [0.837,0.845,0.842,0.808,0.802,0.771]
+correct_secreted = [0.975,0.966,0.972,0.998,1,1] #from false_positives.py
+correct_nonsecreted = [0.837,0.845,0.842,0.808,0.802,0.771] #from false_negatives.py
 
 # Normalize each list by its sum
 normalized_secreted = [x / sum(correct_secreted) for x in correct_secreted]
@@ -9,5 +9,5 @@ normalized_nonsecreted = [x / sum(correct_nonsecreted) for x in correct_nonsecre
 # For example, averaging the normalized values of corresponding positions
 weights = [(ns + nns) / 2 for ns, nns in zip(normalized_secreted, normalized_nonsecreted)]
 
-# Print the resulting weights
+
 print(weights)
